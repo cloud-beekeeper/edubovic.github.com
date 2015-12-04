a787099c39fb33bfe2177c8ada46d985ab2e4a91
Edward Dubovic <edward.dubovic@gmail.com>
2015-12-04 02:01:20 +0200

    update lucru de casa

--- goit/JavaScript/lesson_10-11/public/js/index.js
+++ goit/JavaScript/lesson_10-11/public/js/index.js
@@ -1,42 +1,55 @@
+var GoMath = (function () {
+    function GoMath() {
 
-var GoApp;
-
-function GoMath() {
-    GoMath.prototype.pow = function () {
-        var base, exponent, i, result;
-        base = prompt('Введите число (base)', 1) * 1;
-        result = base;
-        exponent = prompt('Введит степень (exponent)', 1) * 1;
+    }
+    GoMath.prototype.pow = function (base, exponent) {
+        var result = base;
         if (typeof(base) !== 'number' || typeof(exponent) !== 'number' || !((exponent ^ 0) === exponent)) {
-            alert('Error base or exponent');
             return false;
         }
-        if (exponent !== 1) {
-            for (i = 1; i < exponent; i++) {
-                result *= base;
-            }
-        }
-        console.info('Результат: ', result);
-        return true;
-    }
 
-    GoMath.prototype.findByName = function () {
-        var i, element, nameUser
-        mas = [],
-        COUNTER = 4;
-        for (i = 0; i <= COUNTER; i++) {
-            mas[i] = prompt('Введит элемент массива [' + ++i + ']', 'default');
+        if (exponent === 1) {
+            return result;
         }
+        if (exponent === 0 || base === 0) {
+            return 0;
+        }
+        if (base === 1) {
+            return 1;
+        }
+
+        for (var i = 1; i < exponent; i++) {
+            result *= base;
+        }
+
+        return result;
+    };
+
+    return GoMath;
+})();
+
+document.addEventListener('DOMContentLoaded', function () {
+    var GoApp = new GoMath();
+
+    document.getElementById("goApp_pow").onclick = function() {
+        var base, exponent;
+        base = +prompt('Введите число (base)', '1');
+        exponent = +prompt('Введит степень (exponent)', '1');
+
+        console.info('Результат: ', GoApp.pow(base, exponent));
+    };
+
+    document.getElementById("goApp_findUser").onclick = function() {
+        var nameUser, arr = [], COUNTER = 5;
+        for (var i = 0; i < COUNTER ; i++) {
+            arr.push(prompt('Введит элемент массива', 'default'));
+        }
+        console.log(arr);
         nameUser = prompt('Введите имя пользователя', 'user');
-        for (i = 0; i < mas.length; i++) {
-            if (mas[i] === nameUser) {
-                alert(nameUser.concat(', вы успешно вошли'));
-                return true;
-            }
+        if (arr.indexOf(nameUser) == -1) {
+            alert ('Error. User not find');
+            return false;
         }
-        alert ('Error. User not find');
-        return false;
-    }
-}
-
-GoApp = new GoMath();
+        alert(nameUser.concat(', вы успешно вошли'));
+    };
+});
